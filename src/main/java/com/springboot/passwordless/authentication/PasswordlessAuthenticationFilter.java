ed53f03a0b106f43cce8ede3364c0c8bd2dfae06
Sanjana Rajan <sanjana@Sanjanas-MBP.attlocal.net>
2023-10-31 15:52:13 -0500

    Removing unused imports

--- src/main/java/com/springboot/passwordless/authentication/PasswordlessAuthenticationFilter.java
+++ src/main/java/com/springboot/passwordless/authentication/PasswordlessAuthenticationFilter.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
